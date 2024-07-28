500eda3531541fd447037849a6759c48b0447305
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-07-27 22:24:46 -0300

    Atualização

---
+++ main.js
@@ -0,0 +1,9 @@
+// menu
+function toggleMenu(){
+    var menu = document.getElementById("menu");
+    if (menu.style.width === "250px"){
+        menu.style.width = "0";
+    } else {
+     menu.style.width = "250px";
+    }
+}
